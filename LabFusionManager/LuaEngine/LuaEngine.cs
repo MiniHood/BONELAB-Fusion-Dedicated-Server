fc98ae2b4c36da2ac71e5b0d4a72cdc6f701a22c
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-23 18:22:38 +0100

    adding lua functionality for server side scripting

---
+++ LabFusionManager/LuaEngine/LuaEngine.cs
@@ -0,0 +1,28 @@
+﻿using MoonSharp.Interpreter;
+
+public static class LuaEngine
+{
+    private static Script _lua;
+
+    public static Script Lua() => _lua;
+
+    public static void Init()
+    {
+        _lua = new Script();
+
+        UserData.RegisterType<FusionServer>();
+        UserData.RegisterType<ServerManager>();
+
+        _lua.Globals["serverManager"] = ServerManager.Instance;
+    }
+
+    public static DynValue RunScript(string code)
+    {
+        return _lua.DoString(code);
+    }
+
+    public static DynValue RunFile(string path)
+    {
+        return _lua.DoFile(path);
+    }
+}
---
+++ LabFusionManager/LuaEngine/LuaServerAPI.cs
@@ -0,0 +1,15 @@
+﻿public class LuaServerAPI
+{
+    public void Log(string msg) => ServerCLI.AddLog("[Lua] " + msg);
+
+    public List<FusionServer> GetServers() =>
+        ServerManager.Instance.GetAllServers().ToList();
+
+    public async Task SendCommand(FusionServer server, string command, params string[] args)
+    {
+        if (ServerCLI.GetRegisteredCommandNames().Contains(command))
+            await server.SendMessageToClientAsync(command + " " + string.Join(' ', args));
+        else
+            Log($"Unknown command '{command}'");
+    }
+}
