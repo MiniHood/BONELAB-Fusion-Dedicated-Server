2edf340cd51a4ab8043f93b1492b0fd8043992ce
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-23 18:23:03 +0100

    adding lua engine initiation on program

--- LabFusionManager/Program.cs
+++ LabFusionManager/Program.cs
@@ -1,3 +1,10 @@
+﻿using MoonSharp.Interpreter;
+
+// Init lua engine
+UserData.RegisterType<LuaServerAPI>();
+LuaEngine.Lua().Globals["api"] = new LuaServerAPI();
+
+// start pipes and server
 ﻿_ = ServerManager.Instance.StartRegistrationPipeAsync();
 _ = ServerManager.Instance.StartPingLoopAsync();
 Console.WriteLine("Server Manager ready. Registration pipe open.");
