705a8e3160a2ef34bbfa0f396e18e0fec68916f4
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-23 01:58:04 +0100

    Client side communication

---
+++ LabFusion/src/Manager/Client.cs
@@ -0,0 +1,114 @@
+﻿using LabFusion.Data;
+using LabFusion.Network;
+using LabFusion.Player;
+using LabFusion.Utilities;
+using Steamworks.Data;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+
+public class FusionClient
+{
+    public string DisplayName { get; private set; }
+    public string UniqueId { get; private set; }
+
+    private string _clientPipeName;
+    private string _serverPipeName;
+    private Dictionary<string, Func<List<string>, Task<object>>> _asyncCommands = new();
+
+    public FusionClient(string displayName, string uniqueId)
+    {
+        DisplayName = displayName;
+        UniqueId = uniqueId;
+        _clientPipeName = $"LabFusionClientPipe_{uniqueId}";
+        _serverPipeName = "LabFusionServerPipe";
+
+        RegisterCommand("getPlayers", async args =>
+        {
+            var players = LobbyInfoManager.LobbyInfo.PlayerList.Players
+                .Select(playerId => playerId.Username)
+                .ToList();
+
+            return (object)players;
+        });
+    }
+
+    public async Task RegisterAsync()
+    {
+        using var pipe = new NamedPipeClientStream(".", "LabFusionRegistrationPipe", PipeDirection.InOut);
+        await pipe.ConnectAsync();
+
+        string message = $"{DisplayName}:{UniqueId}:{_clientPipeName}";
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        await pipe.WriteAsync(buffer, 0, buffer.Length);
+
+        byte[] responseBuffer = new byte[256];
+        int bytesRead = await pipe.ReadAsync(responseBuffer, 0, responseBuffer.Length);
+        string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+        FusionLogger.Log($"[Client {DisplayName}] Registration response: {response}");
+    }
+
+    public void RegisterCommand<T>(string name, Func<List<string>, Task<T>> action)
+    {
+        _asyncCommands[name] = async (args) => await action(args);
+    }
+
+    public async Task StartListeningAsync()
+    {
+        using var pipeServer = new NamedPipeServerStream(_clientPipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+        FusionLogger.Log($"[Client {DisplayName}] Listening on {_clientPipeName}");
+
+        while (true)
+        {
+            await pipeServer.WaitForConnectionAsync();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = await pipeServer.ReadAsync(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            if (message.Trim() == "ping")
+            {
+                byte[] pong = Encoding.UTF8.GetBytes("pong");
+                await pipeServer.WriteAsync(pong, 0, pong.Length);
+                pipeServer.Disconnect();
+                continue;
+            }
+
+            FusionLogger.Log($"[Client {DisplayName}] Received: {message}");
+
+            var split = message.Split(' ');
+            string commandName = split[0];
+            var args = new List<string>(split.Length > 1 ? split[1..] : Array.Empty<string>());
+
+            if (_asyncCommands.TryGetValue(commandName, out var asyncCommand))
+            {
+                var result = await asyncCommand(args);
+                string json = System.Text.Json.JsonSerializer.Serialize(result);
+                byte[] responseBytes = Encoding.UTF8.GetBytes(json);
+                await pipeServer.WriteAsync(responseBytes, 0, responseBytes.Length);
+            }
+            else
+            {
+                FusionLogger.Log($"[Client {DisplayName}] Unknown command: {commandName}");
+                byte[] responseBytes = Encoding.UTF8.GetBytes("Command received!");
+                await pipeServer.WriteAsync(responseBytes, 0, responseBytes.Length);
+            }
+
+            pipeServer.Disconnect();
+        }
+    }
+
+    public async Task SendMessageAsync(string message)
+    {
+        using var pipeClient = new NamedPipeClientStream(".", _serverPipeName, PipeDirection.InOut);
+        await pipeClient.ConnectAsync();
+
+        byte[] buffer = Encoding.UTF8.GetBytes($"{UniqueId} {message}");
+        await pipeClient.WriteAsync(buffer, 0, buffer.Length);
+
+        byte[] responseBuffer = new byte[256];
+        int bytesRead = await pipeClient.ReadAsync(responseBuffer, 0, responseBuffer.Length);
+        string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+        FusionLogger.Log($"[Client {DisplayName}] Server responded: {response}");
+    }
+}
---
+++ LabFusion/src/Manager/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Command
+{
+    public string Name { get; private set; }
+    public Action<List<string>> Execute { get; private set; }
+
+    public Command(string name, Action<List<string>> execute)
+    {
+        Name = name;
+        Execute = execute;
+    }
+}
