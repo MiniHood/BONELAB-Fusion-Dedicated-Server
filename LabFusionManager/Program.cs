b37c9d7b11512c4d4ad3644dfee060942a67790b
HowNiceOfYou <smellywelly1221@gmail.com>
2025-09-23 01:57:55 +0100

    Added server manager

---
+++ LabFusionManager/Program.cs
@@ -0,0 +1,4 @@
+﻿_ = ServerManager.Instance.StartRegistrationPipeAsync();
+_ = ServerManager.Instance.StartPingLoopAsync();
+Console.WriteLine("Server Manager ready. Registration pipe open.");
+await ServerCLI.StartLiveCommandCLIAsync();
